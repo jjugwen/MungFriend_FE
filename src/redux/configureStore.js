8c7d0b6d1f82b70c24f70708737c110e324057bd
yebin76 <103884098+yebin76@users.noreply.github.com>
2022-06-30 16:47:21 +0900

    Create
    멍프로필 컴포넌트 완성

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import mungSlice from "./modules/mungSlice";
 import myPageSlice from "./modules/myPageSlice";
 
 const store = configureStore({
   reducer: {
-    myPageSlice
+    myPageSlice,
+    mungSlice
   },
 });
 
