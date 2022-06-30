8eee14a1dc70a2ecc17ba22e295528a1d2c8b444
Gwen <yjgwen@gmail.com>
2022-06-30 12:57:44 +0900

    (update) 헤더 버튼 css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import HeaderModal from "./HeaderModal";
 
 function Header() {
@@ -11,31 +12,44 @@ function Header() {
     setOpened(!opened);
   }, [opened]);
 
+  // const [active, setActive] = useState(false);
+  // const HandleButton = (e) => {
+
+  //   setActive = true
+  // }
+
   return (
     <>
-      <div style={{ display: "flex" }}>
+      <HeaderBox>
         <div className="logo">logo</div>
-        <div>
-          <button>서비스 소개</button>
-          <button>산책</button>
-          <button>커뮤니티</button>
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "500px",
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <HeadButton>서비스 소개</HeadButton>
+          <HeadButton>산책</HeadButton>
+          <HeadButton>커뮤니티</HeadButton>
         </div>
         {!token ? (
           <div className="beforeLogin">
-            <button
+            <HeadButton
               onClick={() => {
                 navigate("/login");
               }}
             >
               로그인
-            </button>
-            <button
+            </HeadButton>
+            <HeadButton
               onClick={() => {
                 navigate("/signup");
               }}
             >
               회원가입
-            </button>
+            </HeadButton>
           </div>
         ) : (
           <div className="AfterLogin">
@@ -57,9 +71,25 @@ function Header() {
             </button>
           </div>
         )}
-      </div>
+      </HeaderBox>
     </>
   );
 }
+
+const HeaderBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0% 5%;
+`;
+
+const HeadButton = styled.button`
+  border: none;
+  background-color: transparent;
+
+  &:active {
+    font-weight: 600;
+    text-decoration: underline;
+  }
+`;
 
 export default Header;
