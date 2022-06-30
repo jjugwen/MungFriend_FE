0ebda53836d0581c6f642af330002d315f1a68e7
Gwen <yjgwen@gmail.com>
2022-06-30 11:03:55 +0900

    ci/cd test

--- src/redux/modules/instance.js
+++ src/redux/modules/instance.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: "http://13.125.232.116:8080",
 });
 
-const token = localStorage.getItem("token");
+// const token = localStorage.getItem("token");
 
 // 인스턴스가 생성 된 후 기본값 변경
 // instance.defaults.headers.common["Authorization"] = token
