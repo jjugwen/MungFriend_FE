3c71ab6e1f1da8e0b50c8f7ca4400ded452128a8
Gwen <yjgwen@gmail.com>
2022-07-04 18:08:14 +0900

    Update configureStore.js
    postDetailSlice 스토어 연결

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./modules/userSlice";
 import mungSlice from "./modules/mungSlice";
 import myPageSlice from "./modules/myPageSlice";
+import postDetailSlice from "./modules/postDetailSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     myPageSlice,
     mungSlice,
+    postDetailSlice,
   },
 });
 
