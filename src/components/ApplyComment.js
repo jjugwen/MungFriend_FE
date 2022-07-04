73fee64171a970199b9ad44d4400d44b116faaf1
Gwen <yjgwen@gmail.com>
2022-07-05 00:00:49 +0900

    Update ApplyComment.js
    변수명 일부 업데이트

--- src/components/ApplyComment.js
+++ src/components/ApplyComment.js
@@ -1,35 +1,45 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function ApplyComment() {
+  const detailList = useSelector((state) => state.postDetailSlice.list);
+  // console.log(detailList[0].applyList);
+  const applyList = detailList[0].applyList;
+  // console.log(applyList);
   return (
     <>
       <h1>신청자 댓글</h1>
-      <span>총 2개</span>
+      <span>총 {detailList[0].applyCount}개</span>
       <hr />
-
-      <div className="ApplyCommentBox">
-        <div style={{ display: "flex" }}>
-          <div
-            className="MungProfileImgCircle"
-            style={{
-              width: "48px",
-              height: "48px",
-              backgroundColor: "green",
-              borderRadius: "50%",
-            }}
-          />
-          <div>
-            <div>닉네임</div>
-            <div>1분 전</div>
+      {applyList.map((value) => {
+        // console.log(value);
+        return (
+          <div key={value.id}>
+            <div className="ApplyCommentBox">
+              <div style={{ display: "flex" }}>
+                <div
+                  className="MungProfileImgCircle"
+                  style={{
+                    width: "48px",
+                    height: "48px",
+                    backgroundColor: "green",
+                    backgroundSize: "cover",
+                    borderRadius: "50%",
+                    backgroundImage: `url(${value.dogProfileImgUrl})`,
+                  }}
+                />
+                <div>
+                  <div>{value.nickname}</div>
+                  <div>1분 전</div>
+                </div>
+                <div className="ApplyCommentText">{value.comment}</div>
+                <button>매칭하기</button>
+              </div>
+            </div>
+            <hr />
           </div>
-          <div className="ApplyCommentText">
-            신청자 글입니다. 신청자 글입니다. 신청자 글입니다. 신청자 글입니다.
-            신청자 글입니다. 신청자 글입니다.
-          </div>
-          <button>매칭하기</button>
-        </div>
-      </div>
-      <hr />
+        );
+      })}
     </>
   );
 }
