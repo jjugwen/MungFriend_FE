2b780a8690edd574c69a158bc028e21ece46792f
Gwen <yjgwen@gmail.com>
2022-07-05 10:11:06 +0900

    Create loginAxios.js
    userSlice => loginAxios 파일 이름 변경 (리덕스 부분 삭제)

---
+++ src/redux/modules/loginAxios.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+//미들웨어
+export const loginDB = (username, password) => {
+  // console.log(username, password);
+  return function (dispatch) {
+    axios
+      .post(
+        `http://localhost:5001/member/login`,
+        { username, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            withCredentials: true,
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.status === "true") {
+          // console.log(response);
+          localStorage.setItem("token", response.data.accessToken);
+          window.alert(response.data.message);
+          window.location.replace("/");
+        } else if (response.data.status === "false") {
+          console.log(response.data.status);
+          window.alert(response.data.message);
+          // return
+        }
+      })
+      .catch((err) => {
+        window.alert("에러가 발생했어요!");
+        console.log(err);
+      });
+  };
+};
+
+const actionCreators = { loginDB };
+export { actionCreators };
