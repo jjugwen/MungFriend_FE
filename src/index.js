65fa911b2ac284feb45613a309651599cd5007ea
yebin76 <103884098+yebin76@users.noreply.github.com>
2022-07-05 11:20:34 +0900

    (Create)
    공통 css적용 파일만들기

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./shared/App";
 import "./index.css";
+import "./app.css"
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
