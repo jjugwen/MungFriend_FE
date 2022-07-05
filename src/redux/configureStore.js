bdba408ebff8b63fe6070f06e4e4d00b2c6f066d
Gwen <yjgwen@gmail.com>
2022-07-05 10:07:44 +0900

    Update configureStore.js
    useSlice 불필요해서 스토어에서 삭제함

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import userReducer from "./modules/userSlice";
 import mungSlice from "./modules/mungSlice";
 import myPageSlice from "./modules/myPageSlice";
 import postDetailSlice from "./modules/postDetailSlice";
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
     myPageSlice,
     mungSlice,
     postDetailSlice,
