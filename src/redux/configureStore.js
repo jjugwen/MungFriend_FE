817f0738335fd8ba5c6d05455ecf387239a40944
Gwen <yjgwen@gmail.com>
2022-07-06 00:15:48 +0900

    Update configureStore.js
    userInfoSlice, 스토어에 연결

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import mungSlice from "./modules/mungSlice";
 import myPageSlice from "./modules/myPageSlice";
 import postDetailSlice from "./modules/postDetailSlice";
+import userInfoSlice from "./modules/userInfoSlice";
 
 const store = configureStore({
   reducer: {
     myPageSlice,
     mungSlice,
     postDetailSlice,
+    userInfoSlice,
   },
 });
 
