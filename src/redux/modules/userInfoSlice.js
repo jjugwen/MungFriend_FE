1269c87c05f89665c1817c3711197a31954c9cb3
Gwen <yjgwen@gmail.com>
2022-07-06 00:16:55 +0900

    Create userInfoSlice.js
    작성자, 신청자 확인을 위한 userInfoSlice 생성

---
+++ src/redux/modules/userInfoSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+// import instance from "./instance";
+import axios from "axios";
+
+//미들웨어
+export const myinfoDB = () => {
+  return async function (dispatch) {
+    await axios.get(`http://localhost:5002/myinfo`).then((response) => {
+      //   console.log(response.data);
+      dispatch(getMyInfo(response.data));
+    });
+  };
+};
+
+export const userinfoDB = () => {
+  return async function (dispatch) {
+    // await axios.get(`http://localhost:5002/userinfo/${id}`).then((response) => {
+    await axios.get(`http://localhost:5002/userinfo`).then((response) => {
+      //   console.log(response.data);
+      dispatch(getUserInfo(response.data));
+    });
+  };
+};
+
+//리덕스 toolkit
+export const userInfoSlice = createSlice({
+  name: "myInfo",
+  initialState: {
+    myInfo: [],
+    userInfo: [],
+  },
+  reducers: {
+    getMyInfo(state, action) {
+      state.myInfo = action.payload;
+    },
+    getUserInfo(state, action) {
+      state.userInfo = action.payload;
+    },
+  },
+});
+
+export const actionCreators = { myinfoDB, userinfoDB };
+export const { getMyInfo, getUserInfo } = userInfoSlice.actions;
+export default userInfoSlice.reducer;
