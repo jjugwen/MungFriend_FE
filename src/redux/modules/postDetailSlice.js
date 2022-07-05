ef833534f1e562e79bfbc31c4bd53fab1b169bf7
Gwen <yjgwen@gmail.com>
2022-07-06 00:16:32 +0900

    Update postDetailSlice.js
    initialState 비우기

--- src/redux/modules/postDetailSlice.js
+++ src/redux/modules/postDetailSlice.js
@@ -36,74 +36,7 @@ export const deleteDetailDB = (id) => {
 export const postDetailSlice = createSlice({
   name: "postDetail",
   initialState: {
-    list: [
-      {
-        id: "0",
-        nickname: "작성자",
-        dogProfileImgUrl:
-          "https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_960_720.jpg",
-        title: "게시글 제목",
-        content: "게시글 내용",
-        address: "작성자 주소",
-        requestStartDate: "2022-06-27T12:34",
-        requestEndDate: "2022-06-27T12:38",
-        applyCount: 3,
-        applyByMe: false,
-        isComplete: false,
-        createdAt: "2022-06-26T12:34",
-        modifiedAt: "2022-06-26T15:31",
-        applyList: [
-          {
-            id: 1,
-            nickname: "신청자",
-            comment: "신청자 코멘트",
-            createdAt: "2022-06-26T16:14",
-            modifiedAt: "2022-06-26T19:22",
-            dogProfileImgUrl:
-              "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg",
-          },
-          {
-            id: 2,
-            nickname: "신청자2",
-            comment: "신청자 코멘트2",
-            createdAt: "2022-06-26T22:13",
-            modifiedAt: "2022-06-26T22:22",
-            dogProfileImgUrl:
-              "https://cdn.pixabay.com/photo/2016/07/15/15/55/dachshund-1519374_960_720.jpg",
-          },
-        ],
-        dogList: [
-          {
-            id: 1,
-            name: "멍멍이1",
-            gender: "남",
-            size: "소형",
-            dogImageFiles: [
-              {
-                id: 1,
-                imageUrl:
-                  "https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_960_720.jpg",
-              },
-            ],
-            isRepresentative: true,
-          },
-          {
-            id: 2,
-            name: "멍멍이2",
-            gender: "여",
-            size: "중형",
-            dogImageFiles: [
-              {
-                id: 1,
-                imageUrl:
-                  "https://cdn.pixabay.com/photo/2019/11/07/08/40/puppy-4608266_960_720.jpg",
-              },
-            ],
-            isRepresentative: false,
-          },
-        ],
-      },
-    ],
+    list: [],
   },
   reducers: {
     loadPostDetail(state, action) {
