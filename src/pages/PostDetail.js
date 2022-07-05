81779e6df7d09f73eb9eff874a4cc558318143f7
Gwen <yjgwen@gmail.com>
2022-07-05 13:41:34 +0900

    Update PostDetail.js
    useEffect 적용, mockAPI 테스트 확인

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import ApplyComment from "../components/ApplyComment";
 import styled from "styled-components";
+import { actionCreators as Actions } from "../redux/modules/postDetailSlice";
+import { useParams } from "react-router-dom";
 
 function PostDetail() {
+  const params = useParams();
   const dispatch = useDispatch();
   const detailList = useSelector((state) => state.postDetailSlice.list);
-  console.log(detailList);
-  // console.log(detail_list[0].title);
+  // console.log(detailList);
+  useEffect(() => {
+    dispatch(Actions.getDetailDB(params.id));
+  }, []);
 
   return (
     <>
       <div className="DetailTitleBox" style={{ width: "1000px" }}>
         <div style={{ display: "flex" }}>
-          <div>{detailList[0].isComplete ? "모집중" : "모집종료"} | </div>
-          <div>신청자 {detailList[0].applyList.length}</div>
+          <div>{detailList.isComplete ? "모집종료" : "모집중"} | </div>
+          <div>신청자 {detailList.applyList.length}</div>
         </div>
-        <h1 className="DetailTitle">{detailList[0].title}</h1>
+        <h1 className="DetailTitle">{detailList.title}</h1>
         <div>
           <div
             className="DetailTitleBottom"
@@ -25,26 +30,26 @@ function PostDetail() {
             <div className="DetailTitleBottomStart" style={{ display: "flex" }}>
               <MungProfileImgCircle
                 style={{
-                  backgroundImage: `url(${detailList[0].dogProfileImgUrl})`,
+                  backgroundImage: `url(${detailList.dogProfileImgUrl})`,
                 }}
               />
               <div>
                 <div style={{ display: "flex", gap: "5%" }}>
-                  <div>{detailList[0].nickname}</div>
+                  <div>{detailList.nickname}</div>
                   <div>2km</div>
                 </div>
                 <div>2022.02.04</div>
               </div>
             </div>
             <div className="DetailTitleBottomEnd">
-              요청시간 : {detailList[0].requestStartDate}부터 (1시간)
+              요청시간 : {detailList.requestStartDate}부터 (1시간)
             </div>
           </div>
         </div>
       </div>
       <hr />
       <div className="DetailBodyBox" style={{ height: "300px" }}>
-        {detailList[0].content}
+        {detailList.content}
       </div>
       <hr />
       <div style={{ display: "flex" }}>
