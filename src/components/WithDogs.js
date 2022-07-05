a6e2e553a8f92cc59bd06e9460c0f1a600e3d32a
Gwen <yjgwen@gmail.com>
2022-07-06 00:13:40 +0900

    Create WithDogs.js
    게시글 상세페이지, 선택한 멍친구들(dogList) 컴포넌트 생성

---
+++ src/components/WithDogs.js
@@ -0,0 +1,102 @@
+//게시글 상세페이지 함께하는 멍친구 , 매칭한 프로필 멍멍이 정보 컴포넌트
+
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { actionCreators as Actions } from "../redux/modules/postDetailSlice";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+function DogList() {
+  const params = useParams();
+  const dispatch = useDispatch();
+  const detailList = useSelector((state) => state.postDetailSlice.list);
+  //   console.log(detailList);
+  useEffect(() => {
+    dispatch(Actions.getDetailDB(params.id));
+  }, []);
+
+  return (
+    <Container>
+      <div className="header">
+        <div className="font-20">
+          <b className="DogListTitle">함께하는 멍친구</b>
+        </div>
+      </div>
+      {detailList.dogList?.map((dog) => {
+        return (
+          <div
+            key={dog.id}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              margin: "0 2% auto",
+            }}
+          >
+            <Listbox>
+              <DogImg src={dog.dogImageFiles[0].imageUrl} alt="" />
+              <div>
+                <div className="font-18">
+                  {dog.name} {dog.gender === "여" ? "♀" : "♂"}
+                </div>
+                <div style={{ display: "flex", gap: "5%", width: "100px" }}>
+                  {dog.age}세<div className="font-16">{dog.size}견</div>
+                </div>
+              </div>
+            </Listbox>{" "}
+          </div>
+        );
+      })}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  flex-direction: row;
+  width: 706px;
+  height: 294px;
+  left: 503px;
+
+  .header {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .font-20 {
+    font-size: 20px;
+  }
+  .font-18 {
+    font-size: 18px;
+    font-weight: 600;
+  }
+  .font-16 {
+    font-size: 16px;
+  }
+  .font-14 {
+    font-size: 14px;
+    color: #a4a4a4;
+    padding: 5px;
+    margin-top: 5px;
+  }
+`;
+
+const DogImg = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50px;
+`;
+
+const Listbox = styled.div`
+  width: 900px;
+  height: 80px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  :hover {
+    border: 1px solid black;
+  }
+  border-radius: 12px;
+  box-shadow: 4px 4px 12px rgba(0, 0, 0, 0.04);
+`;
+
+export default DogList;
