a023f420c168f8f3ce69ef20e70b0684c6cd22a1
Gwen <yjgwen@gmail.com>
2022-07-06 16:01:22 +0900

    update sociallogin address

--- src/redux/modules/mungSlice.js
+++ src/redux/modules/mungSlice.js
@@ -4,12 +4,14 @@ import instance from "./instance";
 
 //미들웨어
 //나중에 멤버 아이디값 받아와서 넘겨주기
-export const createMungAX =(mung)=>{
-  return async function(dispatch){
-    await instance.post(`api/dogs`,mung)
-    // await axios.post(`http://localhost:5001/mung`,mung)
-    .then(() => dispatch(createMung(mung)))
-}}
+export const createMungAX = (mung) => {
+  return async function (dispatch) {
+    await instance
+      .post(`/api/dogs`, mung)
+      // await axios.post(`http://localhost:5001/mung`,mung)
+      .then(() => dispatch(createMung(mung)));
+  };
+};
 
 //툴킷
 const mungSlice = createSlice({
