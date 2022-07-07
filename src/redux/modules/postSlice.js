0bf918bd2463a2e816256bb5293ddbf99fe60d46
yebin76 <103884098+yebin76@users.noreply.github.com>
2022-07-07 09:23:43 +0900

    (Create)
    postSlice생성

---
+++ src/redux/modules/postSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import instance from "./instance";
+
+//미들웨어
+//나중에 멤버 아이디값 받아와서 넘겨주기
+export const createPostAX = (post) => {
+  return async function (dispatch) {
+    await instance
+      // .post(`/api/dogs`, post)
+      await axios.post(`http://localhost:5001/post`,post)
+      .then(() => dispatch(createPost(post)));
+  };
+};
+
+//툴킷
+const postSlice = createSlice({
+  name: "post",
+  initialState: { post: {} },
+  reducers: {
+    createPost(state, action) {
+      state.post.push(action.payload);
+    },
+  },
+});
+
+export const { createPost } = postSlice.actions;
+export default postSlice.reducer;
