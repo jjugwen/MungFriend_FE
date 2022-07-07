71361a152560d497677a6c15c53150f5dfe39996
yebin76 <103884098+yebin76@users.noreply.github.com>
2022-07-07 18:19:51 +0900

    (Update)
    멍프로필 load 툴킷 추가

--- src/redux/modules/mungSlice.js
+++ src/redux/modules/mungSlice.js
@@ -6,23 +6,30 @@ import instance from "./instance";
 //나중에 멤버 아이디값 받아와서 넘겨주기
 export const createMungAX = (mung) => {
   return async function (dispatch) {
-    await instance
-      .post(`/api/dogs`, mung)
-      // await axios.post(`http://localhost:5001/mung`,mung)
+    // await instance.post(`/api/dogs`, mung)
+      await axios.post(`http://localhost:5001/mung`,mung)
       .then(() => dispatch(createMung(mung)));
   };
 };
-
+export const loadMyMungAX = ()=>{
+  return async function(dispatch) {
+    await axios.get(`http://localhost:5001/mung`)
+    .then((res)=> dispatch(loadMyMung(res.data)));
+  }
+}
 //툴킷
 const mungSlice = createSlice({
   name: "mung",
-  initialState: { mung: [] },
+  initialState: {},
   reducers: {
     createMung(state, action) {
       state.mung.push(action.payload);
     },
+    loadMyMung(state,action){
+      state.mung = action.payload;
+    }
   },
 });
 
-export const { createMung } = mungSlice.actions;
+export const { createMung, loadMyMung } = mungSlice.actions;
 export default mungSlice.reducer;
