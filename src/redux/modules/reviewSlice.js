2a99408500f575b8f7d7c0c353eae525791473f7
Gwen <yjgwen@gmail.com>
2022-07-09 12:05:47 +0900

    Update reviewSlice.js
    업데이트중

--- src/redux/modules/reviewSlice.js
+++ src/redux/modules/reviewSlice.js
@@ -10,7 +10,7 @@ export const createReviewDB = (data) => {
       .post(`http://localhost:5002/reviews/${data.id}`, {
         comment: data.comment,
         images: data.images,
-        applicantId: data.applicantId,
+        applicantNickname: data.applicantNickname,
         postId: data.id,
       })
       .then((response) => {
@@ -30,14 +30,31 @@ export const createReviewDB = (data) => {
 //redux toolkit
 export const reviewSlice = createSlice({
   name: "reviews",
-  initialState: { list: [] },
+  initialState: {
+    list: [
+      {
+        comment: "후기입니다",
+        images: "imgUrl?",
+        applicantNickname: "산책시켜준 사람 닉네임",
+        postId: 3, //게시글 아이디
+      },
+    ],
+  },
   reducers: {
     reviewCreate(state, action) {
       state.list = action.payload;
+    },
+    reveiewImgCreate(state, action) {
+      console.log(action.payload);
+      state.list.images = action.payload;
+    },
+    reviewImgDelete(state, action) {
+      state.list.images = action.payload;
     },
   },
 });
 
 export const actionCreators = { createReviewDB };
-export const { reviewCreate } = reviewSlice.actions;
+export const { reviewCreate, reveiewImgCreate, reviewImgDelete } =
+  reviewSlice.actions;
 export default reviewSlice.reducer;
