a9371b08fdb5079ea1ad6b2236e485c373b9e29e
Gwen <yjgwen@gmail.com>
2022-07-09 23:17:36 +0900

    Update ApplyComment.js
    유저정보모달창 띄우기 위한 작업1

--- src/components/detail/ApplyComment.js
+++ src/components/detail/ApplyComment.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actionCreators as Actions } from "../../redux/modules/postDetailSlice";
 import { actionCreators as matchActions } from "../../redux/modules/matchingSlice";
+import { actionCreators as userActions } from "../../redux/modules/userInfoSlice";
 import { useParams } from "react-router-dom";
+import UserModal from "../../components/detail/userModal/UserModal";
 
 function ApplyComment() {
   const params = useParams();
@@ -10,6 +12,15 @@ function ApplyComment() {
   const dispatch = useDispatch();
   const detailListroot = useSelector((state) => state.postDetailSlice.list);
   const detailList = detailListroot[postId];
+
+  //신청하기 모달창
+  const [applyModal, setApplyModal] = useState(false);
+  const openApplyModal = () => {
+    setApplyModal(true);
+  };
+  const closeApplyModal = () => {
+    setApplyModal(false);
+  };
 
   useEffect(() => {
     dispatch(Actions.getDetailDB(params.id));
@@ -25,21 +36,48 @@ function ApplyComment() {
           <div key={value.id}>
             <div className="ApplyCommentBox">
               <div style={{ display: "flex" }}>
-                <div
-                  className="MungProfileImgCircle"
-                  style={{
-                    width: "48px",
-                    height: "48px",
-                    backgroundColor: "green",
-                    backgroundSize: "cover",
-                    borderRadius: "50%",
-                    backgroundImage: `url(${value.dogProfileImgUrl})`,
-                  }}
-                />
-                <div>
-                  <div>{value.nickname}</div>
-                  <div>{value.createdAt}</div>
+                <div className="clickUsermodal">
+                  <button
+                    onClick={() => {
+                      dispatch(
+                        userActions.userinfoDB({
+                          nickname: detailList?.nickname,
+                        })
+                      );
+                      setTimeout(() => {
+                        openApplyModal();
+                      }, 500);
+                    }}
+                    style={{ broder: "none", background: "none" }}
+                  >
+                    <div
+                      className="MungProfileImgCircle"
+                      style={{
+                        width: "48px",
+                        height: "48px",
+                        backgroundColor: "green",
+                        backgroundSize: "cover",
+                        borderRadius: "50%",
+                        backgroundImage: `url(${value.dogProfileImgUrl})`,
+                      }}
+                    />
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <div>{value.nickname}</div>
+                      <div>{value.createdAt}</div>
+                    </div>
+                  </button>
+                  <UserModal
+                    children="프로필"
+                    open={applyModal}
+                    close={closeApplyModal}
+                  />
                 </div>
+
                 <div className="ApplyCommentText">{value.comment}</div>
                 {/* isComplete가 false면 매칭하기 버튼 활성화 / true면 비활성화 */}
                 <button
