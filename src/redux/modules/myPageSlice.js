8252996db50ccdc4772c019b3440c94eb11d1e70
yebin76 <103884098+yebin76@users.noreply.github.com>
2022-07-12 20:11:17 +0900

    (Update)
    멍프로필 삭제 (리덕스데이터)

--- src/redux/modules/myPageSlice.js
+++ src/redux/modules/myPageSlice.js
@@ -1,12 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-// import instance from "./instance";
+import instance from "./instance";
+
 
 //미들웨어
 //나중에 멤버 아이디값 받아와서 넘겨주기
 export const loadMyPageAX = (id) => {
   return async function (dispatch) {
-    await axios.get(`http://localhost:5001/mypage`)
+    await instance.post(`/mypage/${id}`)
+    // await axios.get(`http://localhost:5001/mypage`)
     // await instance
       // .get(`/mypage/${id}`)
       .then((response) => dispatch(loadMyPage(response.data)));
@@ -45,9 +47,22 @@ const myPageSlice = createSlice({
     patchPhoneNum(state, action) {
       state.mypage.phoneNum = action.payload.phoneNum;
     },
+    deleteMyMung(state, action){
+      console.log(action.payload);
+      state.mypage.dogList = state
+      .mypage.dogList.filter(
+        (element) => {
+          if (element.id !== action.payload) {
+            return true
+          } else {
+            return false 
+          }
+        }
+      )
+    }
   },
 });
 
-export const { loadMyPage, patchIntroduce, patchPhoneNum } =
+export const { loadMyPage, patchIntroduce, patchPhoneNum, deleteMyMung } =
   myPageSlice.actions;
 export default myPageSlice.reducer;
