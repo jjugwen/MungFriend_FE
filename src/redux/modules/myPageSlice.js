af2dda6bee21ff2d0c1b5226c22caf789cf4ca08
Gwen <yjgwen@gmail.com>
2022-07-15 03:01:36 +0900

    Update myPageSlice.js
    주석 변경/load -get 요청

--- src/redux/modules/myPageSlice.js
+++ src/redux/modules/myPageSlice.js
@@ -2,33 +2,34 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import instance from "./instance";
 
-
-//미들웨어
+//axios
 //나중에 멤버 아이디값 받아와서 넘겨주기
 export const loadMyPageAX = (id) => {
   return async function (dispatch) {
-    await instance.post(`/mypage/${id}`)
-    // await axios.get(`http://localhost:5001/mypage`)
-    // await instance
-      // .get(`/mypage/${id}`)
+    await instance
+      // await axios.get(`http://localhost:5001/mypage`)
+      // await instance
+      .get(`/mypage/${id}`)
       .then((response) => dispatch(loadMyPage(response.data)));
   };
 };
 
 export const patchIntroduceAX = (introduce) => {
   return async function (dispatch) {
-    axios.patch(`http://localhost:5001/mypage`,introduce)
-    // await instance
-    //   .patch(`/mypage/introduce`, introduce)
+    axios
+      .patch(`http://localhost:5001/mypage`, introduce)
+      // await instance
+      //   .patch(`/mypage/introduce`, introduce)
       .then(() => dispatch(patchIntroduce(introduce)));
   };
 };
 
 export const patchPhoneNumAX = (phoneNum) => {
   return function (dispatch) {
-    axios.patch(`http://localhost:5001/mypage`,phoneNum)
-    // instance
-    //   .patch(`/mypage/phoneNum`, phoneNum)
+    axios
+      .patch(`http://localhost:5001/mypage`, phoneNum)
+      // instance
+      //   .patch(`/mypage/phoneNum`, phoneNum)
       .then(() => dispatch(patchPhoneNum(phoneNum)));
   };
 };
@@ -47,19 +48,16 @@ const myPageSlice = createSlice({
     patchPhoneNum(state, action) {
       state.mypage.phoneNum = action.payload.phoneNum;
     },
-    deleteMyMung(state, action){
+    deleteMyMung(state, action) {
       console.log(action.payload);
-      state.mypage.dogList = state
-      .mypage.dogList.filter(
-        (element) => {
-          if (element.id !== action.payload) {
-            return true
-          } else {
-            return false 
-          }
+      state.mypage.dogList = state.mypage.dogList.filter((element) => {
+        if (element.id !== action.payload) {
+          return true;
+        } else {
+          return false;
         }
-      )
-    }
+      });
+    },
   },
 });
 
