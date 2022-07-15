3f86e69600785d029d1fd4d9ebce3f9ca42f56cb
yebin76 <103884098+yebin76@users.noreply.github.com>
2022-07-15 09:36:40 +0900

    (Update)
    후기 css추가

--- src/components/MyReviewList.js
+++ src/components/MyReviewList.js
@@ -1,40 +1,73 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
-import { loadMyPageAX } from "../redux/modules/myPageSlice";
 
 function MyReviewList() {
-  const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    dispatch(loadMyPageAX());
-  }, []);
+
   const info = useSelector((state) => state.myPageSlice.mypage);
 
   return (
     <>
-      <ReviewList>
+      <ReviewList className="column-box">
+      <div className="title">내가 받은 후기</div>
+      <div className="count">
+        총<div className="orange-color">{info?.reviewList.length}</div>건
+      </div>
+        <div className="row-box">
         {info?.reviewList.map((review, i) => {
           return (
-            <div key={i}>
-              <div>{review.writer}</div>
-              <div>{review.comment}</div>
-              <ReviewImg src={review.imagePath} alt="" />
-            </div>
+            <ReviewBox key={i}>
+              <div className="row-box">
+                <GiverImg src={review.giverDogProfileImgUrl} alt=""/>
+                <div className="name-box">
+                  <div>{review.giverNickname}</div>
+                  <div>{review.createdAt.split('T')[0]}</div>
+                </div>
+              </div>
+              <div className="review-box">{review.comment}</div>
+            </ReviewBox>
           );
         })}
+        </div>
       </ReviewList>
     </>
   );
 }
 
 const ReviewList = styled.div`
-  display: flex;
-  flex-direction: row;
+  width: 90%;
+  gap: 4%;
+  .title {
+    font-weight: 600;
+    font-size: 30px;
+    margin-bottom: 30px;
+  }
+  .count {     
+    display: flex;
+    font-weight: 500;
+    .orange-color {
+      color: #fa5a30;
+    }
+  }
 `;
-
-const ReviewImg = styled.img`
-  width: 200px;
-  height: 200px;
+const ReviewBox=styled.div`
+width: 48%;
+height: 216px;
+border-radius: 12px;
+box-shadow: 4px 4px 12px rgba(0, 0, 0, 0.04);
+padding: 10px;
+box-sizing: border-box;
+.name-box{
+  margin-left: 10px;
+}
+.review-box{
+  margin-top: 10px;
+}
+`
+const GiverImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
 `;
 export default MyReviewList;
