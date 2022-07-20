8849a6320525af7eac940f85d22dbfc39098721b
Gwen <yjgwen@gmail.com>
2022-07-20 20:32:45 +0900

    (Update) 전체조회 페이지 수정
    거리순 조회 선택, 모집중 선택,
    대표 멍멍이 없을 시 기본 이미지 노출,
    닉네임 추가,
    게시글 작성 버튼 밑에 고정.

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { loadMyMungAX } from "../redux/modules/mungSlice";
 import styled from "styled-components";
-import { loadPostListAX } from "../redux/modules/postSlice";
+import {
+  loadDistancePostListAX,
+  loadPostListAX,
+} from "../redux/modules/postSlice";
 import Ing from "../assets/images/IsComplete/모집중.svg";
 import Done from "../assets/images/IsComplete/모집종료.svg";
 import { useNavigate } from "react-router-dom";
@@ -13,67 +16,113 @@ function Posts() {
 
   React.useEffect(() => {
     dispatch(loadMyMungAX());
-  }, []);
-
-  React.useEffect(() => {
     dispatch(loadPostListAX());
-  }, []);
+    dispatch(loadDistancePostListAX());
+  }, [dispatch]);
+
   const myMung = useSelector((state) => state.mungSlice.mung);
   // console.log(myMung);
   const Posts = useSelector((state) => state.postSlice.post);
-  // console.log(Posts)
+  // console.log(Posts);
+  const loginNickname = localStorage.getItem("nickname");
+  // console.log(loginNickname)
+  const distancePosts = useSelector((state) =>
+    state.postSlice.distancePost.filter((v) => v.isComplete !== true)
+  );
+  // console.log(distancePosts);
+  const ingPosts = useSelector((state) =>
+    state.postSlice.post.filter((v) => v.isComplete !== true)
+  );
+  // console.log(ingPosts);
 
-  // const a = myMung.isRepresentative
-  // console.log(a)
-
-  // const postImg = Posts.map((p,i)=>p.imagePath.map((p,i)=>(p)))
-  // console.log(postImg)
-
-  // for(let i=0; i<= postImg.length; i++){
-  //   console.log(postImg[i])
-  // }
-
-  // const a = postImg.map((image,i)=> image)
-
-  // console.log(a)
+  //게시글 정렬
+  const [selected, setSelected] = useState(Posts);
+  const handleSelect = (e) => {
+    const value = e.target.value;
+    if (value === "Posts") {
+      setSelected(Posts);
+    }
+    if (value === "distancePosts") {
+      setSelected(distancePosts);
+    }
+    if (value === "ingPosts") {
+      setSelected(ingPosts);
+    }
+  };
 
   return (
     <All>
-      
-    <Box className="row-box">
-        {myMung?.map((dog, i) => {
-          return (
-            <div key={i}>
-              {myMung[i].isRepresentative === true ? (
+      <Box className="row-box">
+        <h1 className="name">
+          {loginNickname}
+          <span>님의</span> <br />
+          {myMung?.length !== 0 ? (
+            <span>멍친구는</span>
+          ) : (
+            <span>멍친구를 등록해주세요!</span>
+          )}
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            marginLeft: "18.5%",
+            paddingTop: "4%",
+            marginTop: "10%",
+          }}
+        >
+          <Kingimg src="https://ifh.cc/g/X36LNp.png" alt="" />
+          {myMung?.map((dog, i) => {
+            return (
+              <div key={i}>
                 <Kingimg src={myMung[i].dogImageFiles[0].imageUrl} alt="" />
-              ) : (
-                ""
-              )}
-              <div className="name">
-                <br />
-                {dog.name} {dog.age}살{/* {dog.length !== 1 ? ",":""} */}
+                {/* isRepresentative === true 인 강아지 filter로 찾으면 되나? */}
+                <p className="dogname">
+                  {dog.name} {dog.age}살
+                  <span style={{ marginRight: "10px" }}> </span>
+                </p>
               </div>
-              
-            </div>
-            
-          );
-        })}
+            );
+          })}
+        </div>
         <SSub>
-        {myMung?.map((dog, i) => {
-          return (
-            <div key={i}>
-              <div className="test"></div>
-              <Subimg src={dog.dogImageFiles[0].imageUrl} />
-            </div>
-          );
-        })}
+          {myMung?.map((dog, i) => {
+            return (
+              <div key={i}>
+                <div className="test"></div>
+                <Subimg src={dog.dogImageFiles[0].imageUrl} />
+              </div>
+            );
+          })}
         </SSub>
       </Box>
-    
-      
-      
+      <div
+        style={{
+          marginTop: "3%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          height: "40px",
+        }}
+      >
+        <h2>산책모집</h2>
+        <select name="게시글" onChange={handleSelect} value={selected}>
+          <option value="1" key="1">
+            게시글 선택
+          </option>
+          <option value="Posts" key="Posts">
+            전체 게시글
+          </option>
+          <option value="ingPosts" key="ingPosts">
+            모집중 게시글
+          </option>
+          <option value="distancePosts" key="distancePosts">
+            가까운 거리순
+          </option>
+        </select>
+      </div>
+
       <Container>
-        {Posts?.map((post, i) => {
+        {selected?.map((post, i) => {
           // console.log(post.imagePath)
           // console.log(post.content.length);
           return (
@@ -95,9 +144,11 @@ function Posts() {
                 </div>
               </div>
               <div className="title">{post.title}</div>
-              <div className="content">{post.content.length > 40?
-              post.content.substr(0,33) : post.content
-            }</div>
+              <div className="content">
+                {post.content.length > 40
+                  ? post.content.substr(0, 33)
+                  : post.content}
+              </div>
               <div className="footer">
                 <hr />
                 <div className="row-box b-box">
@@ -134,34 +185,45 @@ function Posts() {
 }
 
 const All = styled.div`
-  height:100%;
+  height: 100%;
   width: 65%;
   margin: 50px auto auto auto;
 `;
 
-
 const Box = styled.div`
-box-sizing: border-box;
- align-items: center;
+  box-sizing: border-box;
+  align-items: center;
   background: #fa5a30;
   box-shadow: 4px 4px 20px rgba(250, 90, 48, 0.2);
   border-radius: 20px;
   position: relative;
   gap: 13%;
   height: 228px;
- padding: 50px 0 20% 0;
- @media screen and (max-width: 960px) {
-  gap:20%
+  padding: 50px 0 20% 0;
+  @media screen and (max-width: 960px) {
+    gap: 20%;
   }
- /* padding-top: 50px; */
-
+  /* padding-top: 50px; */
+  span {
+    font-weight: 400;
+  }
   .name {
     position: absolute;
     padding-left: 8%;
-    /* padding-bottom: 20%; */
+    padding-top: 10%;
     font-size: 30px;
-    line-height: 40px;
+    font-weight: 600;
+    /* line-height: 40px; */
     color: #ffffff;
+  }
+  .dogname {
+    position: relative;
+    padding-left: 8%;
+    font-size: 30px;
+    font-weight: 600;
+    /* line-height: 40px; */
+    color: #ffffff;
+    width: 100%;
   }
 `;
 const Kingimg = styled.img`
@@ -174,16 +236,15 @@ const Kingimg = styled.img`
 `;
 
 const SSub = styled.div`
-display: flex;
-flex-direction: row;
-position: absolute;
-gap: 10%;
-padding-left:8%;
-padding-bottom: 3%;
-bottom: 0;
-`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  gap: 10%;
+  padding-left: 8%;
+  padding-bottom: 3%;
+  bottom: 0;
+`;
 const Subimg = styled.img`
-  
   border-radius: 50%;
   width: 48px;
   height: 48px;
@@ -239,8 +300,8 @@ const PostBox = styled.div`
       position: relative;
       top: 5px;
     }
-    
-    .b-box{
+
+    .b-box {
       /* display: flex; */
       justify-content: space-between;
     }
@@ -252,14 +313,16 @@ const PostImg = styled.img`
   height: 48px;
 `;
 const AddPostButton = styled.button`
-  position: absolute;
+  position: fixed;
+  bottom: 30px;
+  right: 9%;
+  z-index: 3;
   width: 80px;
   height: 80px;
-  right: 10%;
-  bottom: 40%;
   border: none;
   background-color: #4f65ff;
   border-radius: 50%;
+
   img {
     width: 40px;
     height: 40px;
