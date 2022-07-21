2746c0b735ac80f6a8a100b3a9dd48c0d46eb86c
Gwen <yjgwen@gmail.com>
2022-07-21 23:12:32 +0900

    (Update) 멍프로필 모달창 완성~

--- src/components/detail/withdog/DogProfileViewModal.js
+++ src/components/detail/withdog/DogProfileViewModal.js
@@ -1,163 +1,85 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import Button from "../../../elements/Button";
 
 function DogProfileViewModal(props) {
-  //이미지를 한번 추가해볼게요
-  const navigate = useNavigate();
-  const [mungImage, setMungImage] = useState({
-    image: "",
-    previewUrl: "",
-  });
+  const { open, close, index } = props;
+  //   console.log(index);
+  //멍프로필 정보
+  const mung = useSelector(
+    (state) => state.postDetailSlice.list.dogList[index]
+  );
+  //   console.log(mung[index]);
 
-  const addImage = (event) => {
-    //어떤 이벤트를 명시적으로 처리하지 않은경우,
-    //해당 이벤트에 대한 기본동작을 실행하지 않음
-    event.preventDefault();
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(event.target.files[0]);
-
-    fileReader.onload = (e) => {
-      setMungImage({
-        image: event.target.files[0],
-        previewUrl: e.target.result,
-      });
-      console.log(event.target.files[0]);
-    };
-  };
-  const dispatch = useDispatch();
-  //강아지 정보
-  //age는 숫자로 받아와야 함
-  const [puppy, setpuppy] = useState({ isRepresentative: false });
-  // 강아지 정보 input 입력값 넣어두기
-  //나이는 숫자데이터 . if문 사용해서 숫자로 감싸주기
-  const handleChange = (prop) => (e) => {
-    if (prop !== "age") {
-      setpuppy({ ...puppy, [prop]: e.target.value });
-    } else {
-      setpuppy({ ...puppy, [prop]: Number(e.target.value) });
-    }
-    // console.log(puppy);
-  };
-
-  const signUp = () => {
-    const formData = new FormData();
-    formData.append("image", mungImage.image);
-    const json = JSON.stringify(puppy);
-    const blob = new Blob([json], { type: "application/json" });
-    //infos 추가
-    formData.append("infos", blob);
-    // console.log(formData);
-    // for (const value of formData) console.log(value);
-    //   dispatch(createMungAX(formData));
-    props.setMungModal(!props.modal);
-    // window.location.replace('/mypage')
-    //이미지 서버에 다 보내고 나서 다시 초기값 만들기
-    setMungImage({
-      image: "",
-      previewUrl: "",
-    });
-  };
   return (
-    <Container>
-      <p className="font-24">
-        <b>멍친구 등록</b>
-      </p>
-      <PreviewImg
-        src={
-          mungImage.previewUrl
-            ? mungImage.previewUrl
-            : "https://ifh.cc/g/87kbto.png"
-        }
-        alt=""
-      />
-      <PreviewBtn>
-        <label className="input-button" htmlFor="input-file">
-          <img src="https://ifh.cc/g/ZfSpJc.png" alt="" />
-        </label>
-        <input
-          type="file"
-          accept="images/*"
-          onChange={addImage}
-          id="input-file"
-          className="filebtn"
-        />{" "}
-        {/*스타일 diaplay none으로, 커스텀할려면 라벨링*/}
-      </PreviewBtn>
-      <div className="wrap">
-        <div className="row-box">
-          <div className="column-box">
-            <b>이름</b>
-            <input
-              className="box-size"
-              onChange={handleChange("name")}
-              placeholder="이름을 입력해주세요."
-            />
+    <div className={open ? "openModalcss" : null}>
+      {open ? (
+        <Container>
+          <p className="font-24">
+            <b>멍프로필</b>
+          </p>
+          <PreviewImg
+            src={
+              mung?.dogImageFiles[0]?.imageUrl
+                ? mung?.dogImageFiles[0]?.imageUrl
+                : "https://ifh.cc/g/87kbto.png"
+            }
+            alt=""
+          />
+          <div className="wrap">
+            <div className="row-box">
+              <div className="column-box">
+                <b>이름</b>
+                <div className="box-size">
+                  <p>{mung?.name}</p>
+                </div>
+              </div>
+              <div className="column-box">
+                <b>성별</b>
+                <div className="box-size">
+                  <p>{mung?.gender}</p>
+                </div>
+              </div>
+            </div>
+            <div className="row-box">
+              <div className="column-box ">
+                <b>나이</b>
+                <div className="box-size">
+                  <p>{mung?.age}</p>
+                </div>
+              </div>
+              <div className="column-box">
+                <b>사이즈</b>
+                <div className="box-size">
+                  <p>{mung?.size}</p>
+                </div>
+              </div>
+            </div>
+            <p>
+              <b>몸무게별 사이즈 안내</b>
+            </p>
+            <div className="info-box">
+              ~10키로 : 소형견
+              <br />
+              10~20키로 : 중형견
+              <br />
+              20키로 이상 : 대형견
+              <br />
+            </div>
+            <p>
+              <b>견종이나 유의사항 등 추가할 정보</b>
+            </p>
+            <div className="munginfoText">{mung?.info}</div>
           </div>
-          <div className="column-box">
-            <b>성별</b>
-            <select className="box-size" onChange={handleChange("gender")}>
-              <option value="no">성별을 선택해 주세요</option>
-              <option value="남">남</option>
-              <option value="여">여</option>
-            </select>
+          <div className="btn-box">
+            <Button orange_large _onClick={close}>
+              확인
+            </Button>
           </div>
-        </div>
-        <div className="row-box">
-          <div className="column-box ">
-            <b>나이</b>
-            <input
-              className="box-size"
-              type="number"
-              max="30"
-              onChange={handleChange("age")}
-              placeholder="나이를 입력해주세요."
-            />
-          </div>
-          <div className="column-box">
-            <b>사이즈</b>
-            <select className="box-size" onChange={handleChange("size")}>
-              <option value="no">크기를 선택해 주세요</option>
-              <option value="소형">소형</option>
-              <option value="중형">중형</option>
-              <option value="대형">대형</option>
-            </select>
-          </div>
-        </div>
-        <p>
-          <b>몸무게별 사이즈 안내</b>
-        </p>
-        <div className="info-box">
-          ~10키로 : 소형견
-          <br />
-          10~20키로 : 중형견
-          <br />
-          20키로 이상 : 대형견
-          <br />
-        </div>
-        <p>
-          <b>견종이나 유의사항 등 추가할 정보</b>
-        </p>
-        <textarea
-          onChange={handleChange("info")}
-          placeholder="내용을 입력해주세요."
-        />
-      </div>
-      <div className="btn-box">
-        <button
-          onClick={() => {
-            props.setMungModal(!props.modal);
-          }}
-        >
-          {" "}
-          취소{" "}
-        </button>
-        <button type="submit" onClick={signUp}>
-          확인
-        </button>
-      </div>
-    </Container>
+        </Container>
+      ) : null}
+    </div>
   );
 }
 const Container = styled.div`
@@ -183,7 +105,7 @@ top: 73px; */
     div {
       margin-top: 10px;
     }
-    textarea {
+    .munginfoText {
       box-sizing: border-box;
       width: 445px;
       height: 180px;
@@ -208,6 +130,10 @@ top: 73px; */
     .column-box {
       text-align: left;
       margin: auto;
+
+      p {
+        margin-left: 5%;
+      }
     }
   }
   .box-size {
