cbcef8c51eda44b373647d8f96de197401835b84
Gwen <yjgwen@gmail.com>
2022-07-22 12:03:35 +0900

    (Update) 에러 핸들링 코드 추가중

--- src/redux/modules/instance.js
+++ src/redux/modules/instance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import ErrorNoAccess from "../../components/shared/errors/ErrorNoAccess";
 
 const instance = axios.create({
-  // baseURL: "https://hjkim-sparta.shop",
   baseURL: "https://hjkim-sparta.shop",
+  timeout: 5000,
 });
 
 const token = sessionStorage.getItem("token");
@@ -11,5 +12,43 @@ const token = sessionStorage.getItem("token");
 instance.defaults.headers.common["Authorization"] = token
   ? `Bearer ${token}`
   : null;
+
+// 요청 인터셉터 추가하기
+instance.interceptors.request.use(
+  (config) => {
+    // 요청이 전달되기 전에 작업 수행
+    return config;
+  },
+  (error) => {
+    // 요청 오류가 있는 작업 수행
+    // window.alert(error);
+    // window.alert(error);
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 추가하기
+instance.interceptors.response.use(
+  function (response) {
+    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 데이터가 있는 작업 수행
+    return response;
+  },
+  function (error) {
+    // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 오류가 있는 작업 수행
+    if (error.response.status === 400) {
+      window.alert("인터셉터 400!", error);
+    }
+    if (error.response.status === 401) {
+      window.alert("인터셉터 401!", error);
+    }
+    if (error.response.status === 404) {
+      window.alert("인터셉터 404!", error);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
